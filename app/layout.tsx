01c27edcaee4dd3a4b85666830e356e80dcfef32
oeyoews <jyao4783@gmail.com>
2024-04-03 16:14:42 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
       className="text-black bg-white dark:text-[#cdd9e5] dark:bg-[#1c2128]"
     >
       <body className="antialiased min-h-screen">
+        {/* bg */}
+        <div className="fixed inset-0 -z-50 size-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+          {/* <div className="absolute inset-x-0 top-0 -z-10 m-auto size-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div> */}
+        </div>
+
         <Toaster
           toastOptions={{
             duration: 1000
