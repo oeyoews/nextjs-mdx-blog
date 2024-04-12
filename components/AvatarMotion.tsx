715976e751888d0adb2ff653547dc3b8a53e5a86
oeyoews <jyao4783@gmail.com>
2024-04-12 18:15:41 +0800

    update

--- components/AvatarMotion.tsx
+++ components/AvatarMotion.tsx
@@ -1,11 +1,19 @@
 'use client';
 
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 export default function AvatarMotion() {
+  const [animationKey, setAnimationKey] = useState(0);
+
+  const handleClick = () => {
+    setAnimationKey((prevKey) => prevKey + 1);
+  };
+
   return (
     <motion.div
-      className="size-24 shadow-md bg-cover outline outline-dimmed-100 dark:outline-dimmed-200 bg-center rounded-full inline-block mx-auto bg-avatar-oeyoews"
+      key={animationKey}
+      className="size-24 shadow-md bg-cover outline outline-dimmed-100/80 dark:outline-dimmed-200 bg-center rounded-full inline-block mx-auto bg-avatar-oeyoews cursor-pointer"
       initial={{ scale: 0.5 }}
       animate={{ rotate: 360, scale: 1.0 }}
       transition={{
@@ -13,6 +21,8 @@ export default function AvatarMotion() {
         stiffness: 260,
         damping: 20
       }}
-    />
+      onClick={handleClick}
+      title="点我一下试试"
+    ></motion.div>
   );
 }
