cf6a73ad7f52606292b4300a5a4326d620c04dc7
oeyoews <jyao4783@gmail.com>
2024-04-12 18:32:22 +0800

    feat: support action  for sonner

--- components/AvatarMotion.tsx
+++ components/AvatarMotion.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Toaster, toast } from 'sonner';
+
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 
@@ -8,21 +10,32 @@ export default function AvatarMotion() {
 
   const handleClick = () => {
     setAnimationKey((prevKey) => prevKey + 1);
+    toast.success('在 GitHub 了解更多', {
+      action: {
+        label: '确认',
+        onClick: () => {
+          window.open('https://github.com/oeyoews', '_blank');
+        }
+      }
+    });
   };
 
   return (
-    <motion.div
-      key={animationKey}
-      className="size-24 shadow-md bg-cover outline outline-dimmed-100/80 dark:outline-dimmed-200 bg-center rounded-full inline-block mx-auto bg-avatar-oeyoews cursor-pointer"
-      initial={{ scale: 0.5 }}
-      animate={{ rotate: 360, scale: 1.0 }}
-      transition={{
-        type: 'spring',
-        stiffness: 260,
-        damping: 20
-      }}
-      onClick={handleClick}
-      title="点我一下试试"
-    ></motion.div>
+    <>
+      <Toaster />
+      <motion.div
+        key={animationKey}
+        className="size-24 shadow-md bg-cover outline outline-dimmed-100/80 dark:outline-dimmed-200 bg-center rounded-full inline-block mx-auto bg-avatar-oeyoews cursor-pointer"
+        initial={{ scale: 0.5 }}
+        animate={{ rotate: 360, scale: 1.0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20
+        }}
+        onClick={handleClick}
+        title="点我一下试试"
+      ></motion.div>
+    </>
   );
 }
