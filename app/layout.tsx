6b06d7462646c911fdd11302d2245548bcf88b05
oeyoews <jyao4783@gmail.com>
2024-10-15 14:10:31 +0800

    feat: update viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export const viewport: Viewport = {
 
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1.5,
-  userScalable: true
+  maximumScale: 1,
+  userScalable: false
 };
 
 interface RootLayoutProps {
